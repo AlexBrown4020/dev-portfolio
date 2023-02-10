00bf3aa138b371591560bbe7ee767f3ff92f188e
AlexBrown4020 <balex3497@gmail.com>
2023-02-10 12:03:15 +0900

    Feat: Add more content for footer component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,10 +3,16 @@ import './footer.css';
 function Footer() {
     return (
         <div id='footer'>
-            <h6 id='footer-title'>Information</h6>
-            <p className='footer-p-content'>
-                All Rights Reserved, February 07 2023
-            </p>
+            <section className='footer-info'>
+                <h6 id='footer-title'>Information</h6>
+                <p className='footer-p-content'>February 9th 2023</p>
+                <p className='footer-p-content'>Copyright © 2023, All Rights Reserved.</p>
+            </section>
+            <section className='footer-info'>
+                <h6 id='footer-title'>Contact</h6>
+                <p className='footer-p-content'><strong>Developer:</strong> Alexander Brown</p>
+                <p className='footer-p-content'><strong>Email:</strong> balex3497@gmail.com</p>
+            </section>
         </div>
     )
 };

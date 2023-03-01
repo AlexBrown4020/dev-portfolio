a1f666573f8c4b1dc999f0cd50a3b3404833f4fa
AlexBrown4020 <balex3497@gmail.com>
2023-03-01 16:25:41 +0900

    Feat: Add hover/link to client page

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -5,8 +5,10 @@ function Clients() {
         <section id='clients-container'>
             <h4 id='clients-title'>Client Projects</h4>
             <div className='clients-project-container'>
-                <img href='https://sunset-city.netlify.app/' alt='sunset city logo' src='https://iili.io/HEtcis2.jpg' className='clients-logo'/>
-                <h5 className='client-project-title'>Sunset City</h5>
+                <a href='https://sunset-city.netlify.app/'>
+                    <img  alt='sunset city logo' src='https://iili.io/HEtcis2.jpg' className='clients-logo'/>
+                    <h5 className='client-project-title'>Sunset City</h5>
+                </a>
             </div>
         </section>
     )

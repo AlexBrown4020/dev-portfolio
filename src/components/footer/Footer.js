e86cce880d24abb9f69f7bbdfe3fcfb65032461a
AlexBrown4020 <balex3497@gmail.com>
2023-03-15 11:37:59 +0900

    Refactor: Fix indentation for footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -21,19 +21,19 @@ function Footer() {
                     <p className='footer-p-content'>Copyright © 2023, All Rights Reserved.</p>
                 </section>
             </div>
-                <div id='social-container'>
-                    <div id='social-link-container'>
-                        <a href="https://www.instagram.com/adventurers_hoard/" className="contact-image">
-                            <img alt='instagram' class="contact-image" src="https://iili.io/HENaUmB.png"/>
-                        </a>
-                        <a href="https://www.linkedin.com/in/alex-brown-2a6b7820a/" className="contact-image">
-                            <img alt='linkedin' class="contact-image" src="https://iili.io/HENa85Q.png"/>
-                        </a>
-                        <a href="https://github.com/AlexBrown4020" className="contact-image">
-                            <img alt='github' class="contact-image" src="https://iili.io/HENaO0b.png"/>
-                        </a>
-                    </div>
+            <div id='social-container'>
+                <div id='social-link-container'>
+                    <a href="https://www.instagram.com/adventurers_hoard/" className="contact-image">
+                        <img alt='instagram' class="contact-image" src="https://iili.io/HENaUmB.png"/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/alex-brown-2a6b7820a/" className="contact-image">
+                        <img alt='linkedin' class="contact-image" src="https://iili.io/HENa85Q.png"/>
+                    </a>
+                    <a href="https://github.com/AlexBrown4020" className="contact-image">
+                        <img alt='github' class="contact-image" src="https://iili.io/HENaO0b.png"/>
+                    </a>
                 </div>
+            </div>
         </div>
     );
 };

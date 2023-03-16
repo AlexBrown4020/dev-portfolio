c8e2bdff0271088fa2c01c0c999104ef79522037
AlexBrown4020 <balex3497@gmail.com>
2023-03-17 08:35:21 +0900

    Feat: Change clients container for more detailed description/testimonials

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -4,11 +4,14 @@ function Clients() {
     return (
         <section id='clients-container'>
             <h4 id='clients-title'>Client Projects</h4>
-            <div className='clients-project-container'>
-                <a className='link-container' href='https://sunset-city.netlify.app/'>
-                    <img  alt='sunset city logo' src='https://iili.io/HEtcis2.jpg' className='clients-logo'/>
-                    <h5 className='client-project-title'>Sunset City</h5>
-                </a>
+            <div className='client-project-container'>
+                <h5 className='project-title'>Sunset City</h5>
+                <p className='project-testimonial'>
+                    <a href='https://sunset-city.netlify.app/'><img  alt='sunset city logo' src='https://iili.io/HEtcis2.jpg' className='client-logo'/></a>
+                    "Good work executed in a timely manner.  The website is clear and colourful and fills the requirements I asked for. Feedback during the process was listened to and implemented in a timely manner.  
+                    I have used website building services and the work here met the needs of the project better than a template from one of those sites.  Efficient and exceeds the need of the project, would recommend to friends and collegues."
+                    <h6 className='client-name'>Lauri Halme</h6>
+                </p>
             </div>
         </section>
     );

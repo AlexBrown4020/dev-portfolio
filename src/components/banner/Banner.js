104e9ae7787e3ef9e22b7fe359f6073252c4530b
AlexBrown4020 <balex3497@gmail.com>
2023-03-20 08:54:02 +0900

    Feat: Change main banner content

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -5,6 +5,9 @@ export default function Banner () {
         <main>
             <h1 id='dev-name'>Alexander Brown</h1>
             <h2 className='dev-description'>Software Developer, 3D Designer</h2>
+            <div id='dev-paragraph-container'>
+                <p id='dev-paragraph'>Fullstack software engineer, focusing on agile development and responsive design. Owner of Adventurer's Hoard, 3D design and printing services. </p>
+            </div>
         </main>
     )
 }

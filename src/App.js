64dd3e24df7e6f6df7685b141ace6132ed0cdfe5
AlexBrown4020 <balex3497@gmail.com>
2023-03-20 08:03:27 +0900

    Feat: Change colour scheme

--- src/App.js
+++ src/App.js
@@ -4,15 +4,14 @@ import Footer from './components/footer/Footer';
 import About from './components/about/About';
 import Portfolio from './components/portfolio/Portfolio';
 import Clients from './components/clients/Clients';
+import Banner from './components/banner/Banner';
 
 function App() {
   return (
     <div id="App">
       <div id='home'></div>
       <Navbar/>
-      <div id='banner'>
-        <img id='banner-img' src="https://iili.io/H1Zy9bj.jpg" alt="ABannerTouch" border="0"/>
-      </div>
+      <Banner/>
       <About/>
       <Portfolio/>
       <Clients/>
---
+++ src/components/banner/Banner.js
@@ -0,0 +1,10 @@
+import './banner.css';
+
+export default function Banner () {
+    return (
+        <main>
+            <h1 id='dev-name'>Alexander Brown</h1>
+            <h2 className='dev-description'>Software Developer, 3D Designer</h2>
+        </main>
+    )
+}

a77259f298bc53f5bc752d5c62a03f14e60c0741
AlexBrown4020 <balex3497@gmail.com>
2023-03-20 13:55:12 +0900

    Feat: Update footer date

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -12,7 +12,7 @@ function Footer() {
                 </section>
                 <section className='footer-info'>
                     <h6 className='footer-title'>Information</h6>
-                    <p className='footer-p-content'>Updated: March 17th 2023</p>
+                    <p className='footer-p-content'>Updated: March 20th 2023</p>
                     <p className='footer-p-content'>Copyright © 2023, All Rights Reserved.</p>
                 </section>
             </div>

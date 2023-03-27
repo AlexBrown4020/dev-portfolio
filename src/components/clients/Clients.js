4d16ab2e4d517825e068d0d7a646d4d2d802270d
AlexBrown4020 <balex3497@gmail.com>
2023-03-27 14:15:53 +0900

    Feat: Implement updated client section, make extra room available for additional projects

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,21 +1,44 @@
 import './clients.css';
 
+const clientData = [
+    {
+        title:'Sunset City',
+        description: 'A newly proposed RPG system currently in the development and funding stage, click on the project to see an example of the landing page before public release.',
+        logo: 'https://iili.io/HEtcis2.jpg',
+        testimonial: ' Good work executed in a timely manner.  The website is clear and colourful and fills the requirements I asked for. Feedback during the process was listened to and implemented in a timely manner. I have used website building services and the work here met the needs of the project better than a template from one of those sites.  Efficient and exceeds the need of the project, would recommend to friends and collegues.',
+        name: '- Lauri Halme',
+        link:'https://sunset-city.netlify.app/',
+    },
+    {
+        title: 'Life in Balance',
+        description: 'A landing page for an online yoga platform; allowing users to learn more about the clients products, availability and background.',
+        logo: 'lib.png',
+        testimonial: '',
+        name:'',
+        link:'https://life-in-balance-jb.netlify.app/'
+    }
+]
+
 function Clients() {
     return (
         <section id='clients-container'>
             <h3 className='title navy'>Client Projects</h3>
-            <a className='client-link' href='https://sunset-city.netlify.app/'>
-                <div className='client-project-container'>
-                    <h5 className='client-title'>Sunset City</h5>
-                    <p className='client-description'>A newly proposed RPG system currently in the development and funding stage, click on the project to see an example of the landing page before public release.</p>
-                    <p className='client-testimonial'>            
-                        <img  alt='sunset city logo' src='https://iili.io/HEtcis2.jpg' className='client-logo'/>
-                        " Good work executed in a timely manner.  The website is clear and colourful and fills the requirements I asked for. Feedback during the process was listened to and implemented in a timely manner.  
-                        I have used website building services and the work here met the needs of the project better than a template from one of those sites.  Efficient and exceeds the need of the project, would recommend to friends and collegues. "
-                        <h6 className='client-name'>- Lauri Halme</h6>
-                    </p>
-                </div>
-            </a>
+            {
+                clientData.map(object => {
+                    return  <a className='client-link' href={object.link}>
+                    <div className='client-project-container'>
+                        <h5 className='client-title'>{object.title}</h5>
+                        <p className='client-description'>{object.description}</p>
+                        <p className='client-testimonial'>            
+                            <img  alt='logo' src={object.logo} className='client-logo'/>
+                            {object.testimonial}
+                            <h6 className='client-name'>{object.name}</h6>
+                        </p>
+                    </div>
+                </a>
+                })
+            }
+
         </section>
     );
 };

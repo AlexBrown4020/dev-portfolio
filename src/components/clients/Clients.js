353122d22a91646e938eb08f727aab07f5b2a97a
AlexBrown4020 <balex3497@gmail.com>
2023-05-21 15:07:52 +0900

    Feat: Add client testimonial and name

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -5,7 +5,7 @@ const clientData = [
         title:'Sunset City',
         description: 'A newly proposed RPG system currently in the development and funding stage, click on the project to see an example of the landing page before public release.',
         logo: 'https://iili.io/HEtcis2.jpg',
-        testimonial: ' Good work executed in a timely manner.  The website is clear and colourful and fills the requirements I asked for. Feedback during the process was listened to and implemented in a timely manner. I have used website building services and the work here met the needs of the project better than a template from one of those sites.  Efficient and exceeds the need of the project, would recommend to friends and collegues.',
+        testimonial: '"Good work executed in a timely manner.  The website is clear and colourful and fills the requirements I asked for. Feedback during the process was listened to and implemented in a timely manner. I have used website building services and the work here met the needs of the project better than a template from one of those sites.  Efficient and exceeds the need of the project, would recommend to friends and collegues."',
         name: '- Lauri Halme',
         link:'https://sunset-city.netlify.app/',
     },
@@ -13,8 +13,8 @@ const clientData = [
         title: 'Life in Balance',
         description: 'A landing page for an online yoga platform; allowing users to learn more about the clients products, availability and background.',
         logo: 'lib.png',
-        testimonial: '',
-        name:'',
+        testimonial: '"I recently had a website designed for my yoga company and I found Alex to be knowledgeable and passionate.  He understood exactly what I needed and offered help and ideas.  Alex was organised and efficient with his time scale and had strong communication skills and responded very quickly to any changes that I requested.  He is familiar with the latest design tools and was very quick in designing my website.  I recommend Alex to anyone, thank you from Life in Balance."',
+        name:'- Life in Balance',
         link:'https://life-in-balance-jb.netlify.app/'
     }
 ]

9a6c6265454501af90bda2d878a5ac823ed70ca1
AlexBrown4020 <balex3497@gmail.com>
2024-02-21 01:19:39 +0000

    Feat: Change footer design to alternate with bg

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,13 +7,13 @@ function Footer() {
                 <section className='footer-info'>            
                     <h6 className='footer-title'>About</h6>
                     <p className='footer-p-content'>Freelance developer,</p>
-                    <p className='footer-p-content'>3D Modeler / Designer (Blender),</p>
+                    <p className='footer-p-content'>3D Modeler / Designer,</p>
                     <p className='footer-p-content'>SLA/FDM Printing Services</p>
                 </section>
                 <section className='footer-info'>
                     <h6 className='footer-title'>Information</h6>
-                    <p className='footer-p-content'>Updated: October 10th 2023</p>
-                    <p className='footer-p-content'>Copyright © 2023, All Rights Reserved.</p>
+                    <p className='footer-p-content'>Updated: February 21st 2024</p>
+                    <p className='footer-p-content'>Copyright © 2024, All Rights Reserved.</p>
                 </section>
             </div>
         </footer>
